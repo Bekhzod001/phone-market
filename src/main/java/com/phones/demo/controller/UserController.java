07f2227c610c9bf0013b56574237e9f5dce1cc4f
Bekhzod001 <behzodkeldiyorov2001@gmail.com>
2025-10-01 23:37:16 +0500

    men controller package yaratdim

--- src/main/java/com/phones/demo/controller/UserController.java
+++ src/main/java/com/phones/demo/controller/UserController.java
@@ -1,5 +1,6 @@
-package com.phones.demo;
+package com.phones.demo.controller;
 
+import com.phones.demo.User;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
